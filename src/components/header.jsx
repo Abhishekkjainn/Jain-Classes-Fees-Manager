b99aacb72749d2487ed58fcd98ca0923fcc6cbd4
amarsonspearlsandjewels <amarsons1001@gmail.com>
2025-06-10 14:57:49 +0530

    Added Header

---
+++ src/components/header.jsx
@@ -0,0 +1,20 @@
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="comp">
+        <div className="logo">
+          <img src="/logo2.png" alt="Jain Classes Logo" className="logoimg" />
+        </div>
+        <div className="greeting">
+          <div className="greetingtop">Hello</div>
+          <div className="greetingbottom">Abhishek Jain</div>
+        </div>
+      </div>
+      <div className="actions">
+        <div className="menubutton">
+          <img src="/menu.png" alt="Menu icon" className="menuicon" />
+        </div>
+      </div>
+    </div>
+  );
+}
