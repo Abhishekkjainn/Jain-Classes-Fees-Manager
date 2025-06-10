758e04ecdf06a3ca8d5a50231f8ba764c1603bba
amarsonspearlsandjewels <amarsons1001@gmail.com>
2025-06-10 18:28:50 +0530

    Added Layout

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,3 +1,55 @@
 export default function Dashboard() {
-  return <div className="dashboard">Dashboard</div>;
+  return (
+    <div className="dashboard">
+      <div className="analyticsection">
+        <div className="first">
+          <div className="firstanalytic">
+            <div className="firstanalyticdiv">
+              <div className="firstbtn">
+                <img
+                  src="/money.png"
+                  alt="revenue"
+                  className="firstanalyticicon"
+                />
+              </div>
+            </div>
+            <div className="secondanalyticdiv">₹ 45,000</div>
+            <div className="thirdanalyticdiv">June Month Revenue</div>
+          </div>
+        </div>
+        <div className="second">
+          <div className="secondanalytic">
+            <div className="firstanalyticdiv">
+              <div className="firstbtn secondbtn">
+                <img
+                  src="/graduation.png"
+                  alt="revenue"
+                  className=" sfirstanalyticicon"
+                />
+              </div>
+            </div>
+            <div className="secondanalyticdiv ssecondanalyticdiv">65</div>
+            <div className="thirdanalyticdiv sthirdanalyticdiv">
+              Total Students
+            </div>
+          </div>
+          <div className="secondanalytic">
+            <div className="firstanalyticdiv">
+              <div className="firstbtn secondbtn">
+                <img
+                  src="/money.png"
+                  alt="revenue"
+                  className="firstanalyticicon sfirstanalyticicon"
+                />
+              </div>
+            </div>
+            <div className="secondanalyticdiv ssecondanalyticdiv">₹ 25,000</div>
+            <div className="thirdanalyticdiv sthirdanalyticdiv">
+              Pending Revenue
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
